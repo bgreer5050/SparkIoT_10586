5dda38ab1196c8b714cba3b71bc4c81dba4e993b
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-01-22 07:56:37 -0600

    Added Network Class.

---
+++ SparkRunTime_10586_V1.0/Network.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkRunTime_10586_V1._0
+{
+    public class Network
+    {
+        private string _macAddress;
+        private bool _networkUp;
+        public bool IsNetworkAvailable; //Use NetworkINterface.IsNetworkAvailabe
+
+        public List<string> Errors;
+        public List<string> ObjectMessages;
+
+        public Network()
+        {
+            this.Errors = new List<string>();
+            this.ObjectMessages = new List<string>();
+        }
+        public string MacAddress
+        {
+            get
+            {
+                return "NO MAC ADDRESS FOUND";
+            }
+            set
+            {
+                _macAddress = value;
+            }
+        }
+
+        public bool NetworkUp
+        {
+            get
+            {
+                return NetworkInterface.GetIsNetworkAvailable();
+            }
+            set
+            {
+                _networkUp = value;
+            }
+        }
+
+   
+
+
+    }
+}
