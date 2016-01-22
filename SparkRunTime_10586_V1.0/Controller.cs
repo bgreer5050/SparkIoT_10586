d578d85e31d8ed621206ebb18abdcd9eae0f7cff
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-01-22 07:53:27 -0600

    Added Config and Controller class.  Set to 1090.  Changed Cycle Time and Grace Period Time

---
+++ SparkRunTime_10586_V1.0/Controller.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkRunTime_10586_V1._0
+{
+    public class Controller
+    {
+
+        private string ipAddress;
+        private string modelNumber;
+        private string revision;
+
+        public List<string> Errors;
+        public List<string> ObjectMessages;
+
+        public Controller()
+        {
+            this.modelNumber = "Pi 2";
+            this.Errors = new List<string>();
+            this.ObjectMessages = new List<string>();
+
+        }
+
+
+        public string IPAddress
+        {
+            get { return ipAddress; }
+            set { ipAddress = value; }
+        }
+
+        public string ModelNumber
+        {
+            get { return modelNumber; }
+            set { modelNumber = value; }
+        }
+
+        public string Revision
+        {
+            get { return revision; }
+            set { revision = value; }
+        }
+
+
+
+
+    }
+
+    public class Configuration
+    {
+        private string _assetNumber;
+        private float _gracePeriodMultiple;
+        private int _cycleLengthMs;
+        private int _heartbeatsRequiredToChangeState;
+        private string _enabled;
+
+        public List<string> Errors;
+        public List<string> ObjectMessages;
+
+        public Configuration()
+        {
+            this.AssetNumber = "1090";
+            this._gracePeriodMultiple = 4.0f;
+            this.CycleLengthMs = 20000;
+            this.HeartbeatsRequiredToChangeState = 2;
+
+            this.Errors = new List<string>();
+            this.ObjectMessages = new List<string>();
+
+        }
+
+        public string AssetNumber
+        {
+            get { return _assetNumber; }
+            set { _assetNumber = value; }
+        }
+
+        public string Enabled
+        {
+            get { return _enabled; }
+            set { _enabled = value; }
+        }
+
+        public int HeartbeatsRequiredToChangeState
+        {
+            get { return _heartbeatsRequiredToChangeState; }
+            set { _heartbeatsRequiredToChangeState = value; }
+        }
+
+        public int CycleLengthMs
+        {
+            get { return _cycleLengthMs; }
+            set { _cycleLengthMs = value; }
+        }
+
+        public float GracePeriodMultiple
+        {
+            get
+            {
+                return 4.0f;
+            }
+            set { _gracePeriodMultiple = value; }
+        }
+
+    }
+}
